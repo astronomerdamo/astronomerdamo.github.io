94c9f96763bab2b3fb7ba73739f67cbfc11fef72
Damien Robertson <robertsondamien@gmail.com>
2014-09-05 10:23:54 -0400

    Sorted the quote altText/css issue

--- custom_js/basic_template.js
+++ custom_js/basic_template.js
@@ -14,26 +14,26 @@ function myTime() {
   };
 };
 
-/* Script for humorous alt text on certain words  */
-
 $(document).ready(function() {
   
-  $('.masterQuote').hover(function() {
-    $('.quote').css('display','block');
-  }, function() {
-    $('.quote').css('display','none');
-  });
+  /* Script for humorous alt text on certain words  */
   
-  /*$('.masterQuote').hover(function(){
+  $('.masterQuote').hover(function(){
       var altText = $(this).attr('altText');
-      $('<p class="quote"></p>').html(altText).appendTo('.container').fadeIn('fast');
+      $('<p class="quote"></p>').html(altText).appendTo('body').fadeIn('slow');
+      var quoteBackgroundColor = $('.navbar-collapse').css('background-color');
+      var quoteColor = $('h3').css('color');
+      $('.quote').css('background-color', quoteBackgroundColor);
+      $('.quote').css('color', quoteColor);
     },function() {
-      $('.quote').remove();
-    }).mousemove(function(e) {
-      var mx = e.pageX + 12;
+      $('.quote').fadeOut('slow', function() {
+        $(this).remove();
+      });
+  }).mousemove(function(e) {
+      var mx = e.pageX + 0;
       var my = e.pageY + 12;
       $('.quote').css({ top: my, left: mx });
-    });*/
+    });
   
   /* Colour rules for Solarized Light */
   
@@ -47,8 +47,6 @@ $(document).ready(function() {
     $('hr').css('border-color', '#93a1a1');
     $('code').css('background-color', '#eee8d5');
     $('pre').css('background-color', '#eee8d5');
-    $('.quote').css('background-color', '#eee8d5');
-    $('.quote').css('color', '#586e75');
     $('.btn-default').hover(function() {
       $(this).css('background-color', '#eee8d5');
     }, function() {
@@ -81,8 +79,6 @@ $(document).ready(function() {
     $('hr').css('border-color', '#586e75');
     $('code').css('background-color', '#073642');
     $('pre').css('background-color', '#073642');
-    $('.quote').css('background-color', '#073642');
-    $('.quote').css('color', '#93a1a1');
     $('.btn-default').hover(function() {
       $(this).css('background-color', '#073642');
     }, function() {
